21a6e8bfac52f28336562a5256b649c792d3c511
Shreyasi08 <47986235+Shreyasi08@users.noreply.github.com>
2025-03-20 20:34:14 -0700

    Create Food Balances.sql

---
+++ DDL/Food Balances.sql
@@ -0,0 +1,19 @@
+-- auto-generated definition
+create table foodbalances_cle_2
+(
+    area_code    integer,
+    area         text,
+    item_code    integer,
+    item         text,
+    element_code integer,
+    element      text,
+    unit         text,
+    year_2020    double precision,
+    year_2021    double precision,
+    year_2022    double precision,
+    year_2023    double precision
+);
+
+alter table foodbalances_cle_2
+    owner to postgres;
+
