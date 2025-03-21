669b30166293c19ee4a0f2909ac40264886e4f86
Shreyasi08 <47986235+Shreyasi08@users.noreply.github.com>
2025-03-20 20:32:53 -0700

    Create Price.sql

---
+++ DDL/Price.sql
@@ -0,0 +1,48 @@
+-- auto-generated definition
+create table price_cle_2
+(
+    area         text,
+    area_code    integer,
+    item         text,
+    item_code    integer,
+    element      text,
+    element_code integer,
+    unit         text,
+    y2020        double precision,
+    y2021        double precision,
+    y2022        double precision,
+    y2023        double precision,
+    y1991        double precision,
+    y1992        double precision,
+    y1993        double precision,
+    y1994        double precision,
+    y1995        double precision,
+    y1996        double precision,
+    y1997        double precision,
+    y1998        double precision,
+    y1999        double precision,
+    y2000        double precision,
+    y2001        double precision,
+    y2002        double precision,
+    y2003        double precision,
+    y2004        double precision,
+    y2005        double precision,
+    y2006        double precision,
+    y2007        double precision,
+    y2008        double precision,
+    y2009        double precision,
+    y2010        double precision,
+    y2011        double precision,
+    y2012        double precision,
+    y2013        double precision,
+    y2014        double precision,
+    y2015        double precision,
+    y2016        double precision,
+    y2017        double precision,
+    y2018        double precision,
+    y2019        double precision
+);
+
+alter table price_cle_2
+    owner to postgres;
+
